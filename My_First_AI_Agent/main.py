376b42fbce3e8fe3ae698af7c590402434d4ba3e
Muhammad Hasnain <tryfrelancer23@gmail.com>
2025-05-27 14:41:53 +0500

    just build my very first AI gents that can have memory both user and agents and the power of gemini model I also added openAI stream_events() so that user can see a nice looking typing (also called as streaming animation)

---
+++ My_First_AI_Agent/main.py
@@ -0,0 +1,65 @@
+from agents import (
+    Agent,
+    Runner,
+    RunConfig,
+    AsyncOpenAI,
+    OpenAIChatCompletionsModel,
+)
+from openai.types.responses import ResponseTextDeltaEvent
+import os
+from dotenv import load_dotenv
+import random
+import chainlit as cl
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash", openai_client=external_client
+)
+
+config = RunConfig(model=model, model_provider=external_client, tracing_disabled=True)
+
+agent = Agent(
+    name="GIAIC Agent",
+    instructions="Hello! I'm your AI assistant.",
+    model=model,
+)
+
+
+@cl.on_chat_start
+async def handle_start():
+    cl.user_session.set("history", [])
+    await cl.Message(
+        content="Welcome to the Hasnain AI Assistent, What I can help you today!"
+    ).send()
+
+
+@cl.on_message
+async def handle_massage(message: cl.Message):
+    history = cl.user_session.get("history")
+
+    msg = cl.Message(content="")
+    await msg.send()
+
+    history.append({"role": "user", "content": message.content})
+    result = Runner.run_streamed(
+        agent,
+        input=history,
+        run_config=config,
+    )
+
+    async for event in result.stream_events():
+        if event.type == "raw_response_event" and isinstance(
+            event.data, ResponseTextDeltaEvent
+        ):
+            await msg.stream_token(event.data.delta)
+    history.append({"role": "assistant", "content": result.final_output})
+    cl.user_session.set("history", history)
+    # await cl.Message(content=result.final_output).send()
